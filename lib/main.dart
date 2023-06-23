8285421dfdfc8d7c9c05ab11bd6d861c987af1db
ShankarKakumani <shankarchowdary7545@gmail.com>
2023-06-24 00:12:52 +0530

    - hosted on firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:shankar_kakumani/presentation/views/work_in_porgress_screen.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(const MyApp());
 }
 
