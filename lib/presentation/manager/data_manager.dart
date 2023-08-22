66323717ebecada3255c8ee0ed18b1828a9daa89
ShankarKakumani <shankarchowdary7545@gmail.com>
2023-08-22 21:23:17 +0530

    - added audio player support

--- lib/presentation/manager/data_manager.dart
+++ lib/presentation/manager/data_manager.dart
@@ -1,4 +1,4 @@
 class DataManager {
   static const String backgroundMusicUrl = jePresqueUrl;
-  static const String jePresqueUrl = "https://firebasestorage.googleapis.com/v0/b/shankar-kakuman1.appspot.com/o/J'ai%20Presque%20Peur.mp3?alt=media&token=a2b2d65c-f4cc-4cf8-a04b-4b6316af0d37";
+  static const String jePresqueUrl = "https://firebasestorage.googleapis.com/v0/b/shankar-kakuman1.appspot.com/o/Jai%20Presque%20Peur.mp3?alt=media&token=f44b6bd1-a1a2-4f39-85f2-b9f44ac8b6c6";
 }
